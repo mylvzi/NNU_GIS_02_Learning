d1aeec46a11ec30694ac260cd166256e17944fe9
mylvzi <2314394028@qq.com>
2025-04-08 23:25:23 +0800

    线性表实现

--- demo3.c
+++ demo3.c
@@ -1,1 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<string.h>
+
+/**
+	leetcode刷题总结
+*/
+//int removeElement(int* nums, int n, int val) {
+//    int  slow = -1, fast = 0;
+//    while (fast < n) {
+//        if (nums[fast] != val)
+//            nums[++slow] = nums[fast++];
+//        else ++fast;
+//    }
+//    return slow + 1;
+//}
+//
+//void duplicateZeros(int* arr, int n) {
+//    int zero_cnt = 0, l = 0;
+//    while (l < n)
+//        if (arr[l++] == 0)
+//            ++zero_cnt;
+//
+//    int i = n - 1, j = n + zero_cnt - 1;
+//    while (i >= 0) {
+//        if (j < n) arr[j] = arr[i];
+//        if (arr[i] == 0) {
+//            j--;
+//            if (j < n) arr[j] = 0;
+//        }
+//
+//        j--; i--;
+//    }
+//}
+
+/**
+	顺序表re
+*/
+typedef unsigned int ui;
+int main() {
+	ui i = 0;
+	printf("%d\n", i);
+	return 0;
+}
+
+
+/**
+	实现线性表  list
+*/
+//int main() {
+//
+//}
