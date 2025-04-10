87204844035baf40692eb9ac5811edb3b63c3b17
mylvzi <2314394028@qq.com>
2025-04-10 21:50:49 +0800

    SL exercise

--- demo4.c
+++ demo4.c
@@ -61,27 +61,166 @@
 typedef int SLDataType;
 
 #include <assert.h>
-// 初始化
+//// 初始化
+//void SLInit(SL* ps) {
+//	assert(ps);
+//	ps->arr = (SLDataType*)malloc(10 * sizeof(SLDataType));
+//	if (ps->arr == NULL) {
+//		perror("fail to malloc");
+//		exit(-1);
+//	}
+//
+//	ps->capacity = 10;
+//	ps->size = 0;
+//}
+//
+//void SLDestroy(SL* ps) {
+//	assert(ps);
+//	free(ps->arr);
+//	ps->arr = NULL;
+//
+//	ps->capacity = ps->size = 0;
+//}
+//
+//void SLPrint(SL* ps) {
+//	assert(ps);
+//	for (int i = 0; i < ps->size; ++i) {
+//		printf("%d ", ps->arr[i]);
+//	}
+//}
+//
+//// 尾插
+//void SLPushBack(SL* ps, SLDataType n) {
+//	assert(ps);
+//	SLCheckCapacity(ps);
+//
+//	ps->arr[ps->size] = n;
+//	ps->size++;
+//
+//	// 直接复用
+//	// SLInsert(ps, ps->size, n);
+//}
+//
+//// 尾删  直接让有效数据个数--即可  后续添加也会直接覆盖
+//void SLPopBack(SL* ps) {
+//	assert(ps);
+//	assert(ps->size > 0);// assert是里面成立不报警  不成立才报警
+//
+//	// 不为空
+//	ps->size--;
+//}
+//
+//// 头插
+//void SLPushFront(SL* ps, SLDataType n) {
+//	assert(ps);
+//	SLCheckCapacity(ps);
+//
+//	// 保证不会溢出
+//	for (int i = ps->size; i > 0; i--) {
+//		ps->arr[i] = ps->arr[i - 1];
+//	}
+//	ps->arr[0] = n;
+//	ps->size++;
+//
+//	// 复用
+//	// SLInsert(ps, 0, n);
+//}
+//
+//// 头删
+//void SLPopFront(SL* ps) {
+//	assert(ps);
+//	assert(ps->size > 0);
+//
+//	for (int i = 0; i < ps->size - 1; ++i) {
+//		ps->arr[i] = ps->arr[i + 1];
+//	}
+//	ps->size--;
+//}
+//
+//// 在pos位置插入x(pos就是数组下标)
+//void SLInsert(SL* ps, int pos, SLDataType x) {
+//	assert(ps);
+//	assert(pos >= 0 && pos <= ps->size);
+//	SLCheckCapacity(ps);
+//
+//	for (int i = ps->size; i > pos; --i) {
+//		ps->arr[i] = ps->arr[i - 1];
+//	}
+//	ps->arr[pos] = x;
+//	ps->size++;
+//}
+//
+//// 在pos位置删除
+//void SLErase(SL* ps, int pos) {
+//	assert(ps);
+//	assert(pos >= 0 && pos < ps->size);
+//	assert(ps->size > 0);
+//
+//	for (int i = pos; i < ps->size - 1; ++i)
+//		ps->arr[i] = ps->arr[i + 1];
+//	ps->size--;
+//}
+//
+//// 判断顺序表中是否含有n
+//int SLFind(SL* ps, SLDataType n) {
+//	assert(ps);
+//
+//	for (int i = 0; i < ps->size; ++i)
+//		if (ps->arr[i] == n)
+//			return i;
+//	return 0;// 不包含
+//}
+//
+////修改pos位置的值为x
+//void SLModify(SL* ps, int pos, SLDataType x) {
+//	assert(ps);
+//	assert(pos >= 0 && pos < ps->size);
+//
+//	ps->arr[pos] = x;
+//}
+//
+//
+//
+//
+//
+//
+//// 检查是否需要扩容
+//void SLCheckCapacity(SL* ps) {
+//	assert(ps);
+//	if (ps->size == ps->capacity) {
+//		int newCapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;// 防止有人没有初始化
+//		SLDataType* tmp = (SLDataType*)realloc(ps->arr, newCapacity * sizeof(SLDataType));
+//		if (tmp == NULL) {
+//			perror("fail to realloc");
+//			exit(-1);
+//		}
+//
+//		ps->arr = tmp;
+//		ps->capacity = newCapacity;
+//	}
+//}
+
+
+//初始化顺序表
 void SLInit(SL* ps) {
-	assert(ps);
-	ps->arr = (SLDataType*)malloc(10 * sizeof(SLDataType));
+	ps->arr = (SLDataType*)malloc(4 * sizeof(SLDataType));
 	if (ps->arr == NULL) {
 		perror("fail to malloc");
 		exit(-1);
 	}
-
-	ps->capacity = 10;
 	ps->size = 0;
+	ps->capacity = 4;
 }
 
+//删除顺序表
 void SLDestroy(SL* ps) {
 	assert(ps);
 	free(ps->arr);
 	ps->arr = NULL;
-
-	ps->capacity = ps->size = 0;
+	ps->size = ps->capacity = 0;
 }
 
+//打印顺序表
 void SLPrint(SL* ps) {
 	assert(ps);
 	for (int i = 0; i < ps->size; ++i) {
@@ -89,52 +228,27 @@ void SLPrint(SL* ps) {
 	}
 }
 
+/**
+	增删改查
+*/
 // 尾插
 void SLPushBack(SL* ps, SLDataType n) {
-	assert(ps);
-	SLCheckCapacity(ps);
-
-	ps->arr[ps->size] = n;
-	ps->size++;
-
-	// 直接复用
-	// SLInsert(ps, ps->size, n);
+	SLInsert(ps, ps->size, n);
 }
 
-// 尾删  直接让有效数据个数--即可  后续添加也会直接覆盖
+// 尾删
 void SLPopBack(SL* ps) {
-	assert(ps);
-	assert(ps->size > 0);// assert是里面成立不报警  不成立才报警
-
-	// 不为空
-	ps->size--;
+	SLErase(ps, ps->size - 1);
 }
 
 // 头插
 void SLPushFront(SL* ps, SLDataType n) {
-	assert(ps);
-	SLCheckCapacity(ps);
-
-	// 保证不会溢出
-	for (int i = ps->size; i > 0; i--) {
-		ps->arr[i] = ps->arr[i - 1];
-	}
-	ps->arr[0] = n;
-	ps->size++;
-
-	// 复用
-	// SLInsert(ps, 0, n);
+	SLInsert(ps, 0, n);
 }
 
 // 头删
 void SLPopFront(SL* ps) {
-	assert(ps);
-	assert(ps->size > 0);
-
-	for (int i = 0; i < ps->size - 1; ++i) {
-		ps->arr[i] = ps->arr[i + 1];
-	}
-	ps->size--;
+	SLErase(ps,0);
 }
 
 // 在pos位置插入x(pos就是数组下标)
@@ -143,8 +257,8 @@ void SLInsert(SL* ps, int pos, SLDataType x) {
 	assert(pos >= 0 && pos <= ps->size);
 	SLCheckCapacity(ps);
 
-	for (int i = ps->size; i > pos; --i) {
-		ps->arr[i] = ps->arr[i - 1];
+	for (int i = ps->size - 1; i >= pos; --i) {
+		ps->arr[i + 1] = ps->arr[i];
 	}
 	ps->arr[pos] = x;
 	ps->size++;
@@ -154,21 +268,20 @@ void SLInsert(SL* ps, int pos, SLDataType x) {
 void SLErase(SL* ps, int pos) {
 	assert(ps);
 	assert(pos >= 0 && pos < ps->size);
-	assert(ps->size > 0);
+	assert(ps->size >= 0);
 
-	for (int i = pos; i < ps->size - 1; ++i)
+	for (int i = pos; i < ps->size - 1; ++i) {
 		ps->arr[i] = ps->arr[i + 1];
+	}
 	ps->size--;
 }
 
 // 判断顺序表中是否含有n
 int SLFind(SL* ps, SLDataType n) {
-	assert(ps);
-
 	for (int i = 0; i < ps->size; ++i)
 		if (ps->arr[i] == n)
-			return i;
-	return 0;// 不包含
+			return 1;
+	return 0;
 }
 
 //修改pos位置的值为x
@@ -179,17 +292,12 @@ void SLModify(SL* ps, int pos, SLDataType x) {
 	ps->arr[pos] = x;
 }
 
-
-
-
-
-
-// 检查是否需要扩容
+// 判断是否扩容
 void SLCheckCapacity(SL* ps) {
 	assert(ps);
 	if (ps->size == ps->capacity) {
-		int newCapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;// 防止有人没有初始化
-		SLDataType* tmp = (SLDataType*)realloc(ps->arr, newCapacity * sizeof(SLDataType));
+		int newCapacity = ps->capacity == 0 ? 4 : 2 * ps->capacity;
+		SLDataType* tmp = (SLDataType*)realloc(ps->arr, 2 * newCapacity * sizeof(SLDataType));
 		if (tmp == NULL) {
 			perror("fail to realloc");
 			exit(-1);
@@ -199,6 +307,7 @@ void SLCheckCapacity(SL* ps) {
 		ps->capacity = newCapacity;
 	}
 }
+
 
 int main() {
 	SL s;
